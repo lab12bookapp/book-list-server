167e99707c0b3bfbe6b393c51c382fb9cd29ba31
Laboni A <asthanal@yahoo.com>
2018-04-21 09:48:04 -0700

    changes to server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 // API Endpoints
 app.get('/api/v1/books', (req, res) => {
-  client.query('SELECT book_id, title, author, image_url, isbn FROM books;')
+  client.query('SELECT title, author, image_url, isbn FROM books;')
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
